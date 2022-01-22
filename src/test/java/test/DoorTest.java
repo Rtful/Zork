873b433f0dc0a52b7c29e71e8b79b4c89b5a682a
Aaron Holenstein <4aron.dodger@gmail.com>
2022-01-22 13:16:39 +0100

    Reworked Rooms and Doors - added Navigator.java Class that now manages all rooms and interactions with them. This is to clean up the game class - added 2d array rooms that contains all rooms in a grid - changed Room.java to no longer contain other rooms as exits but instead doors - changed navigation to now be based on coordinates (the directions are interpreted as changes in x and y) - added Lock.java that can either have an Item as a key or String - added and rewrote tests as necessary, though InventoryTest and NavigatorTest are both empty so far - made minor syntax changes suggested by IntelliJ IDE

---
+++ src/test/java/test/DoorTest.java
@@ -0,0 +1,70 @@
+package test;
+
+import ch.bbw.zork.Door;
+import ch.bbw.zork.Item;
+import ch.bbw.zork.Lock;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DoorTest {
+    private Door door;
+    private Item key;
+    private String code;
+    private Lock stringLock;
+    private Lock objectLock;
+
+    @Before
+    public void init(){
+        this.key = new Item("key", "a key");
+        this.code = "1234";
+        this.stringLock = new Lock(this.code, "a lock that needs a code to open");
+        this.objectLock = new Lock(this.key, "a lock that needs a key to open");
+    }
+
+    @Test
+    public void testUnlockStringCode() {
+        this.door = new Door(this.stringLock);
+        assertTrue(this.door.unlock(this.code));
+    }
+
+    @Test
+    public void testLockIsOpenStringCode(){
+        this.door = new Door(this.stringLock);
+        this.door.unlock(code);
+        assertTrue(this.door.isOpen());
+    }
+
+    @Test
+    public void testLockIsNotOpenStringCode(){
+        this.door = new Door(stringLock);
+        assertFalse(this.door.isOpen());
+    }
+
+    @Test
+    public void testUnlockObjectKey() {
+        this.door = new Door(objectLock);
+        assertTrue(this.door.unlock(this.key));
+    }
+
+    @Test
+    public void testLockIsOpenObjectKey(){
+        this.door = new Door(objectLock);
+        this.door.unlock(this.key);
+        assertTrue(this.door.isOpen());
+    }
+
+    @Test
+    public void testLockIsNotOpenObjectKey(){
+        this.door = new Door(objectLock);
+        assertFalse(this.door.isOpen());
+    }
+
+    @Test
+    public void testHasNoLock(){
+        this.door = new Door(null);
+        assertTrue(this.door.isOpen());
+    }
+}
